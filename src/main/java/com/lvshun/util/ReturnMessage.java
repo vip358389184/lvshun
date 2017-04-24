80e19a21056247dfbba7978c74c6035174e5e465
于文扬 <Matthew@yuwenyangdeMBP.AirDream>
2017-04-24 18:01:26 +0800

    first lvshun

---
+++ src/main/java/com/lvshun/util/ReturnMessage.java
@@ -0,0 +1,28 @@
+package main.java.com.lvshun.util;
+
+
+/**
+ * Created by Administrator on 2017/2/17 0017.
+ */
+public enum  ReturnMessage {
+
+    SUCCESS("SUCCESS", "成功"),
+    ERROR("ERROR","失败")
+    ;
+    private String code;
+
+    private String message;
+
+    private ReturnMessage(String code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
