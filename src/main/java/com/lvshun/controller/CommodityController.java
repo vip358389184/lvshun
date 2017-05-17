2864b3407d6ae99a0624081846c3367ae689b51c
dingbaolong <Dblshine@163.com>
2017-05-17 16:22:39 +0800

    商品图片上传

--- src/main/java/com/lvshun/controller/CommodityController.java
+++ src/main/java/com/lvshun/controller/CommodityController.java
@@ -6,20 +6,25 @@ import com.lvshun.po.Commodity;
 import com.lvshun.util.FileNameUtil;
 import com.lvshun.util.ResponseUtil;
 import com.lvshun.util.ReturnMessage;
+import com.lvshun.util.UUIDUtil;
 import com.lvshun.vo.Commoditys;
 import com.lvshun.vo.User;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 /**
@@ -42,26 +47,26 @@ public class CommodityController {
     @ResponseBody
     @RequestMapping(value="/addComm")
     public ReturnMessage addComm(@RequestBody Commoditys  commoditys){
-
-        logger.info(commoditys);
+        logger.error(commoditys.getPicture());
+        commoditys.setCommodity_id(UUIDUtil.getUUID());
         commodityBiz.addComm(commoditys);
 
         return  ReturnMessage.SUCCESS;
     }
 
-     /**
+    /**
      * 删除用户
      * @param request
      * @param model
      *
      */
-     @ResponseBody
+    @ResponseBody
     @RequestMapping("/delete")
-        public String delete( HttpServletRequest request,Model model)throws Exception{
-       String id= request.getParameter("id");
+    public String delete( HttpServletRequest request,Model model)throws Exception{
+        String id= request.getParameter("id");
 
-            int delCol = commodityBiz.delete(id);
-            model.addAttribute(delCol);
+        int delCol = commodityBiz.delete(id);
+        model.addAttribute(delCol);
 
         return "";
     }
@@ -74,52 +79,13 @@ public class CommodityController {
         Commodity commList = commodityBiz.findById(id);
         return  commList;
     }
-//修改
-@ResponseBody
-@RequestMapping("/update")
- public String update(HttpServletRequest request,Commodity commoditys){
+    //修改
+    @ResponseBody
+    @RequestMapping("/update")
+    public String update(HttpServletRequest request,Commodity commoditys){
 
         commodityBiz.update(commoditys);
         return "";
- }
- //添加图片
- @RequestMapping("/updateIcon")
- @ResponseBody
- public String updateIcon(@RequestParam(value = "file", required = false) MultipartFile file, String id, HttpServletRequest request){
-     String reply="";
-     if(file.getSize()==0){
-         return "no file";
-     }
-     String path=request.getSession().getServletContext().getRealPath("upload");
-     //获取上传文件的名字
-//		String oriFileName = file.getOriginalFilename();
-     //重新起名
-     String fileName = FileNameUtil.getName()+".jpg";
-//		String requestUrl = request.getRequestURL().toString();
-//		String location = requestUrl.substring(0, requestUrl.indexOf("aps"));
-     try {
-         if("success".equals(reply)){
-             File targetFile = new File(path, fileName);
-             if(!targetFile.exists()){
-                 targetFile.mkdirs();
-             }
-
-             //保存
-             try {
-                 file.transferTo(targetFile);
-                 reply="success";
-             } catch (Exception e) {
-                 e.printStackTrace();
-             }
-         }
-         //			add.setName(new String(add.getName().getBytes("ISO-8859-1"), "utf-8"));
-
-     } catch (Exception e) {
-         e.printStackTrace();
-         reply="更新信息失败！具体异常信息：" + e.getMessage();
-     } finally {
-         return reply;
-     }
- }
+    }
 
 }
--- src/main/java/com/lvshun/controller/FileUploadController.java
+++ src/main/java/com/lvshun/controller/FileUploadController.java
@@ -1,10 +1,10 @@
 package com.lvshun.controller;
 
-import com.sun.java.swing.plaf.windows.resources.windows;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -22,27 +22,33 @@ import java.util.Iterator;
 @Controller
 @RequestMapping(value = "/FileUpload")
 public class FileUploadController {
-    @RequestMapping(value = "fileUpload_ajax", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @RequestMapping(value = "fileUpload_ajax",method = RequestMethod.POST)
     @ResponseBody
-    public String upload( MultipartFile file, HttpServletRequest request, ModelMap model) {
-        System.out.println("开始");
-        String path = request.getSession().getServletContext().getRealPath("upload");
-        String fileName = file.getOriginalFilename();
-        System.out.println(path);
-        File targetFile = new File(path, fileName);
-        if(!targetFile.exists()){
-            targetFile.mkdirs();
-        }
-        //保存
-        try {
-            file.transferTo(targetFile);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        model.addAttribute("fileUrl", request.getContextPath()+"/upload/"+fileName);
+    public String springUpload(HttpServletRequest request,@RequestParam("file_AjaxFile") MultipartFile file1) throws IllegalStateException, IOException {
+        /**
+         * 进行判断是否存在文件
+         **/
+        if (!file1.isEmpty()) {
+            /**
+             * 设置文件放置路径
+             *  根目录下的 upload
+             */
+            String filePath = request.getSession().getServletContext().getRealPath("/") + "upload/"
+                    + file1.getOriginalFilename();
+            // 转存文件
+            //原来为   临时文件 需要转存才可以保存下来
+            file1.transferTo(new File(filePath));
 
-        return "/success";
+/**
+ * 数据库 存储地址
+ *   "upload/"+ file1.getOriginalFilename();
+ *   如未防止 文件名重复 可采用  UUId进行防止
+ */
+            String dbfilePath = "upload/" + file1.getOriginalFilename();
+            return "{data:'"+dbfilePath+"'}";
+
+        }
+        return "{'data':fail}";
     }
-
 }
 
